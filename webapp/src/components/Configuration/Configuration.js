21ea6357c56d3e70d062bb2ce3523aee3257cd50
Tosha Kamath <tosha.kamath@ibm.com@Toshas-MacBook-Pro.local>
2020-10-20 17:23:58 -0700

    Config UI design

--- webapp/src/components/Configuration/Configuration.js
+++ webapp/src/components/Configuration/Configuration.js
@@ -7,17 +7,22 @@ import {
   Link,
   Form,
   Button,
+  RadioButton,
+  RadioButtonGroup,
+  FormGroup,
+  TextArea,
+  FileUploader,
 } from "carbon-components-react";
 import axios from "axios";
 
 const { PasswordInput } = TextInput;
 
 export default function Configuration() {
-  const [clusterName, setClusterName] = useState("some cluster name?");
-  const [apiKey, setApiKey] = useState("some apiKey?");
-  const [resourceGroup, setResourceGroup] = useState("some resourceGroup?");
-  const [publicVLAN, setPublicVLAN] = useState("some publicVLAN?");
-  const [privateVLAN, setPrivateVLAN] = useState("some privateVLAN?");
+  const [moduleName, setModuleName] = useState("default-module-name");
+  // const [apiKey, setApiKey] = useState("some apiKey?");
+  // const [resourceGroup, setResourceGroup] = useState("some resourceGroup?");
+  // const [publicVLAN, setPublicVLAN] = useState("some publicVLAN?");
+  // const [privateVLAN, setPrivateVLAN] = useState("some privateVLAN?");
 
   const [regionAPIResponseOptions] = useState([
     {
@@ -37,10 +42,21 @@ export default function Configuration() {
       value: "Option 3",
     },
   ]);
+
+  const allSourceFileTypes = ["Python", "Bash"];
+  const allExtensions = ["py", "sh"];
+  const allOS = ["Darwin", "Linux"];
+  const alltypesOfRun = ["Parallel", "Synchronous"];
+
   const [region, setRegion] = useState(regionAPIResponseOptions[0]);
 
   const [modules, setModules] = useState([""]);
   let [selectedModule] = useState(modules.length > 0 ? [modules[0]] : [""]);
+
+  const [sourceFileTypes, setSourceFileTypes] = useState(allSourceFileTypes)
+  const [extensions, setExtensions] = useState(allExtensions)
+  const [os, setOS] = useState(allOS)
+  const [typeOfRun, setTypeOfRun] = useState(alltypesOfRun)
 
   //   const updateFieldChanged = e => {
   //     console.log("hey: ",items);
@@ -52,12 +68,12 @@ export default function Configuration() {
 
   const makeSampleAPICall = (e) => {
     const data = {
-      clusterName: clusterName,
-      apiKey: apiKey,
-      resourceGroup: resourceGroup,
-      publicVLAN: publicVLAN,
-      privateVLAN: privateVLAN,
-      region: region.value,
+      moduleName: moduleName,
+      // apiKey: apiKey,
+      // resourceGroup: resourceGroup,
+      // publicVLAN: publicVLAN,
+      // privateVLAN: privateVLAN,
+      // region: region.value,
       selectedModule: selectedModule,
     };
     console.log("options: ", region);
@@ -97,45 +113,175 @@ export default function Configuration() {
       </div>
       <div>
         <Form>
-          <FormLabel>
-            <Tooltip triggerText="Cluster name">
-              Create a suitable name for your cluster.
-            </Tooltip>
-          </FormLabel>
-          <TextInput
-            id="ibmConfiguration-textInput-clusterName"
-            placeholder="Cluster name"
-            defaultValue={clusterName}
-            onChange={(e) => {
-              console.log("clustername: ", e.target.value);
-              setClusterName(e.target.value);
-            }}
-          />
+          <FormGroup>
+            <FormLabel>
+              <Tooltip triggerText="Select whether you want to create a new module or run an existing module">
+                Create a suitable name for your module.
+              </Tooltip>
+            </FormLabel>
+            <RadioButtonGroup
+              // defaultSelected="default-selected"
+              valueSelected={(e) => {
+                console.log("check: ", e);
+              }}
+              legend="Group Legend"
+            >
+              <RadioButton
+                value="default-selected"
+                labelText="existing module"
+                id="existing"
+                // checked={(e) => {
+                //   console.log(e.checked);
+                // }}
+                // onChange={(e) => {
+                //   console.log(e.target.value);
+                // }}
+              />
+              {/* <RadioButton value="standard" labelText="new module" id="new" /> */}
+            </RadioButtonGroup>
+          </FormGroup>
 
-          <FormLabel>
-            <Tooltip triggerText="Region">
-              <p id="tooltip-body">IBM Cloud locations.</p>
-              <div className="bx--tooltip__footer">
-                <Link
-                  href="https://cloud.ibm.com/docs/containers?topic=containers-regions-and-zones"
-                  target="_blank"
-                >
-                  Learn more
-                </Link>
-              </div>
-            </Tooltip>
-          </FormLabel>
-          <Dropdown
-            items={modules}
-            label="Select a region"
-            defaultValue={modules[0]}
-            onChange={(e) => {
-              selectedModule = e.selectedItem;
-              console.log(e.selectedItem);
-            }}
-          />
+          {/* <FormGroup>
+            <FormLabel>
+              <Tooltip triggerText="New Module">
+                Create a suitable name for your module.
+              </Tooltip>
+            </FormLabel>
+            <TextInput
+              id="ibmConfiguration-textInput-moduleName"
+              placeholder="Enter new module name"
+              onChange={(e) => {
+                console.log("moduleName: ", e.target.value);
+                setModuleName(e.target.value);
+              }}
+            />
+          </FormGroup> */}
 
-          <FormLabel>
+          <FormGroup>
+            <FormLabel>
+              <Tooltip triggerText="Module">
+                <p id="tooltip-body">IBM Cloud locations.</p>
+                <div className="bx--tooltip__footer">
+                  <Link
+                    href="https://cloud.ibm.com/docs/containers?topic=containers-regions-and-zones"
+                    target="_blank"
+                  >
+                    Learn more
+                  </Link>
+                </div>
+              </Tooltip>
+            </FormLabel>
+            <Dropdown
+              items={modules}
+              label="Select an existing module"
+              defaultValue={modules[0]}
+              onChange={(e) => {
+                selectedModule = e.selectedItem;
+                console.log(e.selectedItem);
+              }}
+            />
+          </FormGroup>
+
+          <FormGroup>
+            <TextArea placeholder="Paste JSON here or upload json file"></TextArea>
+            {/* <Button kind="ghost">Upload JSON file</Button> */}
+            <FileUploader
+              multiple
+              accept={[".json"]}
+              buttonKind="ghost"
+              buttonLabel="Upload .json files at 500mb or less"
+              filenameStatus="edit"
+            />
+          </FormGroup>
+
+          <FormGroup>
+            <FormLabel>
+              <Tooltip triggerText="Type of execution">
+                Shows what scripts will run, but does not run the scripts
+              </Tooltip>
+            </FormLabel>
+            <RadioButtonGroup
+              // defaultSelected="default-selected"
+              valueSelected={(e) => {
+                console.log("check: ", e);
+              }}
+            >
+              <RadioButton
+                value="default-selected"
+                labelText="dry run"
+                id="d"
+              />
+              <RadioButton value="standard" labelText="run" id="r" />
+            </RadioButtonGroup>
+          </FormGroup>
+
+          <FormGroup>
+            <FormLabel>
+              <Tooltip triggerText="Source file type">
+                Select source file type [python, bash]
+              </Tooltip>
+            </FormLabel>
+            <Dropdown
+              items={sourceFileTypes}
+              label="Select your source file type"
+              defaultValue={sourceFileTypes[0]}
+              onChange={(e) => {
+                selectedModule = e.selectedItem;
+                console.log(e.selectedItem);
+              }}
+            />
+          </FormGroup>
+
+          <FormGroup>
+            <FormLabel>
+              <Tooltip triggerText="File extension">
+                Select file extension [py, sh]
+              </Tooltip>
+            </FormLabel>
+            <Dropdown
+              items={extensions}
+              label="Select your file extension"
+              defaultValue={extensions[0]}
+              onChange={(e) => {
+                selectedModule = e.selectedItem;
+                console.log(e.selectedItem);
+              }}
+            />
+          </FormGroup>
+          <FormGroup>
+            <FormLabel>
+              <Tooltip triggerText="OS">
+                Select OS [Darwin, Linux]
+              </Tooltip>
+            </FormLabel>
+            <Dropdown
+              items={os}
+              label="Select your OS"
+              defaultValue={os[0]}
+              onChange={(e) => {
+                selectedModule = e.selectedItem;
+                console.log(e.selectedItem);
+              }}
+            />
+          </FormGroup>
+          <FormGroup>
+            <FormLabel>
+              <Tooltip triggerText="Type of Run">
+                Select type of run [Parallel, Synchronous]
+              </Tooltip>
+            </FormLabel>
+            <Dropdown
+              items={typeOfRun}
+              label="Select your type of run"
+              defaultValue={typeOfRun[0]}
+              onChange={(e) => {
+                selectedModule = e.selectedItem;
+                console.log(e.selectedItem);
+              }}
+            />
+          </FormGroup>
+
+          {/* <FormLabel>
             <Tooltip triggerText="API key">
               <p id="tooltip-body">IBM Cloud user API key.</p>
               <div className="bx--tooltip__footer">
@@ -230,7 +376,7 @@ export default function Configuration() {
               console.log("privateVLAN: ", e.target.value);
               setPrivateVLAN(e.target.value);
             }}
-          />
+          /> */}
         </Form>
 
         <div style={{ marginTop: "2%" }}>
