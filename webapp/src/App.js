4fae9aa860c2e6f4d153eb2acbd38b2aeadd7ce0
Tosha Kamath <tosha.kamath@ibm.com@Toshas-MacBook-Pro.local>
2020-11-04 17:04:26 -0800

    render tabview on execution

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,4 +1,4 @@
-import React, {useReducer} from 'react';
+import React from 'react';
 import {
   HashRouter as Router,
   Switch,
@@ -7,15 +7,9 @@ import {
 // Need to use hashrouter to serve as static html file
 
 import './App.scss';
-import Configuration from './components/Configuration/Configuration';
 import TabView from './components/TabView/TabView';
-import Install from './components/Status/Install';
-import { NOTIFICATION_INIT, NOTIFICATION_REDUCER } from './constants/reducer-constants';
-
 
 export default function App() {
-  const [notification, notificationDispatch] = useReducer(NOTIFICATION_REDUCER, NOTIFICATION_INIT);
-
 
   return (
     <Router>
