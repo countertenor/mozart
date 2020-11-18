368ce7e87b266728a262150f1542d607d0d75e89
Tosha Kamath <tosha.kamath@ibm.com@Toshas-MacBook-Pro.local>
2020-11-18 13:30:33 -0800

    code cleanup

--- webapp/src/components/Status/Install.js
+++ webapp/src/components/Status/Install.js
@@ -1,13 +1,11 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
-import { useHistory, useParams } from 'react-router-dom';
 import {
   Button, CodeSnippet, InlineLoading, Loading, Modal, Tile, Accordion, AccordionItem
 } from 'carbon-components-react';
 import { CheckmarkFilled16, Misuse16, View16 } from '@carbon/icons-react';
 import styles from './Install.module.scss';
 import { getStatus, setupWS } from './InstallUtils';
-import axios from "axios";
 
 function LogModal({ onRequestClose, open, log }) {
   return (
@@ -95,11 +93,9 @@ Task.propTypes = {
   openLogModal: PropTypes.func.isRequired,
 };
 
-export default function Install(props, { notificationDispatch, uninstall }) {
+export default function Install(props) {
   const intervalRef = useRef(null);
   const logRef = useRef('');
-  const history = useHistory();
-  const { provider } = useParams();
   const [steps, setSteps] = useState([]);
   const [state, setState] = useState("");
   let stringPrint=" - "
@@ -108,7 +104,6 @@ export default function Install(props, { notificationDispatch, uninstall }) {
   }
   const [print] = useState(stringPrint)
   const [percentage, setPercentage] = useState({});
-  // const [totalStepsCount, setTotalNumberOfSteps] = useState({});
   
   // steps is an array of objects with keys directory, module, tasks
   // tasks is an array of objects with keys taskName and status
@@ -152,83 +147,32 @@ export default function Install(props, { notificationDispatch, uninstall }) {
   }, []);
 
   const getData = useCallback(async () => {
-    // let obj ={};
     let countObj={}
     let moduleName = props.getModuleName()
     getStatus(moduleName, (err, data) => {
       if (err) {
         console.log(err)
-        // notificationDispatch({
-        //   type: 'setNotification',
-        //   notification: {
-        //     title: 'Error',
-        //     subtitle: 'Unable to fetch the current status, will attemp to retry.',
-        //     caption: '',
-        //     kind: 'error',
-        //   }
-        // });
       }
       setSteps((data||{}).steps||[]);
       setState((data||{}).state||"")
       console.log("hello",(data||{}).steps||[])
       setCurStatus(''); // TODO: Figure out
       ((data||{}).steps||[]).map( e => {
-
         countObj = {...countObj,
             [e.module]: 0
         }
         let count = 0;
         e.tasks.map(item =>{
-
           if(item.status.state === "success"){
             countObj = {...countObj,
               [e.module]: Math.trunc(++count * 100/e.tasks.length)
             }
           }
-          
         })
       })
       setPercentage(countObj);
-      // setTotalNumberOfSteps(obj);
     });
-    // switch (resCurStatus) {
-    // case 'complete':
-    //   if (uninstall) {
-    //     history.push('/uninstallcomplete');
-    //   } else {
-    //     history.push('/complete'); // We will let the cleanup function handle the interval (see useEffect(() => {}, []))
-    //   }
-    //   break;
-    // case 'failed':
-    //   notificationDispatch({
-    //     type: 'setNotification',
-    //     notification: {
-    //       title: 'Error',
-    //       subtitle: `${uninstall ? 'Uninstall' : 'Install'} failed. An error has occured while ${uninstall ? 'uninstalling' : 'installing'}. You can either restart or go back to re-enter your configuration. Err: ${response.data.errMsg}.`,
-    //       caption: '',
-    //       kind: 'error',
-    //     }
-    //   });
-    //   clearStatusInterval();
-    //   break;
-    // case 'waiting':
-    //   notificationDispatch({
-    //     type: 'setNotification',
-    //     notification: {
-    //       title: 'Progress stalled',
-    //       subtitle: 'A step has been completed that requires the user to restart the script to continue.',
-    //       caption: '',
-    //       kind: 'info',
-    //     }
-    //   });
-    //   clearStatusInterval();
-    //   break;
-    // default:
-    //   break;
-    // }
-    // setStatuses(response.data.stepItems);
-    // setCurStatus(response.data.status.toLowerCase());
-  }, [uninstall, clearStatusInterval, history, notificationDispatch]);
+  }, [clearStatusInterval]);
 
 
   const cancel = (e) =>{
@@ -251,33 +195,11 @@ export default function Install(props, { notificationDispatch, uninstall }) {
       console.log(`interval already set at: ${intervalRef.current}, clearing`);
       clearInterval(intervalRef.current);
     }
-
     const intervalId = setInterval(getData, 3000);
     console.log(`set interval: ${intervalId}`);
     intervalRef.current = intervalId;
   }, [getData]);
-
-  // const restart = useCallback(async () => {
-  //   setCurStatus('loading');
-  //   if (uninstall) {
-  //     await restartUninstall(provider); // TODO: error handling for everything
-  //   } else {
-  //     await restartProvision(provider); // TODO: error handling for everything
-  //   }
-  //   notificationDispatch({
-  //     type: 'setNotification',
-  //     notification: {
-  //       title: 'Success',
-  //       subtitle: `${uninstall ? 'Uninstall' : 'Installation'} has restarted successfully.`,
-  //       caption: '',
-  //       kind: 'success',
-  //       settimeout: 3
-  //     }
-  //   });
-  //   getData();
-  //   setStatusInterval(); // this handles clearing for us
-  // }, [uninstall, setStatusInterval, getData, notificationDispatch, provider]);
-
+  
   useEffect(() => {
     getData();        //gets data.steps array of tasks
     setStatusInterval();
@@ -306,7 +228,6 @@ export default function Install(props, { notificationDispatch, uninstall }) {
                 return (
                   <div key={module.module} className={styles.module}>
                     <AccordionItem title= {module.module+print+percentage[module.module]+"% complete"}>
-                      {/* <p>{totalStepsCount[module.module]}</p> */}
                       {module.tasks.map((task) => (
                         <ul
                           className={styles.loadersHolder}
@@ -328,40 +249,10 @@ export default function Install(props, { notificationDispatch, uninstall }) {
               })}
             </Accordion>
             <br />
-            {/* (curStatus === 'failed' || curStatus === 'waiting') && (
-            <div id="install-restartPromptHolder" className={styles.restartPromptHolder}>
-              {curStatus === 'failed' ? (
-                <>
-                  <p>{`There was an error ${uninstall ? 'when uninstalling' : 'in the installation'}.`}</p>
-                  <p>You can view this issue in detail in the log file: nz-cloud/nz_install_[timestamp].log.</p>
-                </>
-              ) : (
-                <p>{`The ${uninstall ? 'uninstall' : 'installer'} requires the user to continue the process.`}</p>
-              )}
-              <div className="ButtonRow">
-                <Button
-                  id="install-goback-button"
-                  onClick={() => history.goBack()}
-                  kind="secondary"
-                >
-                  Back
-                </Button>
-                <Button
-                  id="install-restart-button"
-                  onClick={restart}
-                >
-                  {curStatus === 'failed' ? 'Restart' : 'Continue'}
-                </Button>
-              </div>
-            </div>
-              ) */}
             <div className="ButtonRow">
               <Button onClick={cancel} kind="secondary">
                 Cancel
               </Button>
-              {/* <Button onClick={() => history.push("/enclosureConfig")}>
-                Next
-              </Button> */}
             </div>
           </>
         )}
@@ -374,11 +265,3 @@ export default function Install(props, { notificationDispatch, uninstall }) {
     </div>
   );
 }
-
-Install.defaultProps = {
-  uninstall: false
-};
-
-Install.propTypes = {
-  uninstall: PropTypes.bool
-};
