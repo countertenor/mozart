c066ec73dd0263022cd45a58622199d8e8c6e5e6
Prashant Gupta <prashantgupta24@gmail.com>
2021-03-19 17:06:18 -0700

    removing module

--- resources/templates/test-module/20-random-module/00-module1/python-11.py
+++
@@ -1,5 +0,0 @@
-import time
-
-print "Start : %s" % time.ctime()
-time.sleep( 3 )
-print "End : %s" % time.ctime()
--- resources/templates/test-module/20-random-module/00-module1/python-13.py
+++
@@ -1,5 +0,0 @@
-import time
-
-print "Start : %s" % time.ctime()
-time.sleep( 2 )
-print "End : %s" % time.ctime()
