fdd3f03d941f1a1fbcdaa975de6d92d4510e6cb6
Prashant Gupta <prashantgupta24@gmail.com>
2021-03-29 10:16:36 -0700

    missed a print

--- internal/command/command-center.go
+++ internal/command/command-center.go
@@ -262,7 +262,6 @@ func GetAllDirsInsideTmpl() ([]string, error) {
 //returns number of files cleaned up, along with error (if nil)
 func cleanupFilesInDir(directory string) (int, error) {
 	filesDeleted := 0
-	fmt.Println("directory : ", directory)
 	err := filepath.Walk(directory, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
